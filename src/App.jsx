9a6629db3457da59e9bdcac4788d383ad3ba4332
Julia Varjamo <juliavarjamo@Julia---MacBook-Air.local>
2024-01-20 20:36:00 +0200

    Added contacts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import React from 'react';
 import NavigationBar from './components/Navigation.jsx';
-import HomePage from './components/HomePage.jsx';
 
 function App() {
   return (
--- src/components/ContactsPage.jsx
+++ src/components/ContactsPage.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import '../Navigation.css';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import SmartphoneOutlinedIcon from '@mui/icons-material/SmartphoneOutlined';
+import AlternateEmailOutlinedIcon from '@mui/icons-material/AlternateEmailOutlined';
+import './Contacts.css';
 
 function ContactsPage() {
     return (
         <div>
-            <h2>contacts page </h2>
+            <div className='header'>
+                <h2>Contact me</h2>
+            </div>
+            <div className='contacts'>
+                <div className='location'>
+                    <LocationOnOutlinedIcon/>
+                    <p>Tampere, Finland</p>
+                </div>
+                <div className='email'>
+                    <EmailOutlinedIcon />
+                    <p>leevi7.oksanen@gmail</p>
+                </div>
+                <div className='phone'>
+                    <SmartphoneOutlinedIcon />
+                    <p>+358 445088115</p>
+                </div>
+                <div className='social'>
+                    <AlternateEmailOutlinedIcon />
+                    <p>leevioksanenn</p>
+                </div>  
+            </div>      
         </div>
     );
 };
